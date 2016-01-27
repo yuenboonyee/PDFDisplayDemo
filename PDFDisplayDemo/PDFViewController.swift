7ec170e8755342ee649a16a926fdb61f9d6a4c96
Yuen Boon Yee <yuen_boon_yee@mac.com>
2016-01-27 17:16:46 +1100

    Completed demo.

--- PDFDisplayDemo/PDFViewController.swift
+++ PDFDisplayDemo/PDFViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class PDFViewController: UIViewController {
     @IBOutlet weak var uiWebView: UIWebView!
+    @IBOutlet weak var uiActivityIndicator: UIActivityIndicatorView!
+    var nsData:NSData = NSData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +20,9 @@ class PDFViewController: UIViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-//        let urlRequest = NSURLRequest(URL: NSURL(string: "https://www.google.com")!)
-        let urlRequestPDF = NSURLRequest(URL: NSURL(string: "https://auspost.com.au/media/documents/domestic-parcels-guide-jan15.pdf")!)
-//        self.uiWebView.loadRequest(urlRequest)
-        self.uiWebView.loadRequest(urlRequestPDF)
-
+//        let urlRequestPDF = NSURLRequest(URL: NSURL(string: "https://auspost.com.au/media/documents/domestic-parcels-guide-jan15.pdf")!)
+//        self.uiWebView.loadRequest(urlRequestPDF)
+        self.getNSDataForPDF()
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,15 +31,39 @@ class PDFViewController: UIViewController {
     }
     
     
+    //MARK: Private methods
+    /**
+    Uses an NSURLSession to retrieve the PDF data. Updates the UIActivityIndicator when done. Sets the nsData property.
+    */
+    func getNSDataForPDF(){
+        let nsURLSession = NSURLSession.sharedSession()
+        let nsURL = NSURL(string: "https://auspost.com.au/media/documents/domestic-parcels-guide-jan15.pdf")
+        //        let nsURL = NSURL(fileURLWithPath: "https://auspost.com.au/media/documents/domestic-parcels-guide-jan15.pdf")
+        let nsURLRequest = NSURLRequest(URL: nsURL!)
+        let nsURLSessionDataTask = nsURLSession.dataTaskWithRequest(nsURLRequest) { (nsData, nsURLResponse, nsError) -> Void in
+            self.nsData = nsData!
+            //Update UI in the main thread. Ref: http://stackoverflow.com/questions/28137380/updating-ui-from-background-thread-swift
+            dispatch_async(dispatch_get_main_queue()) {
+                // update some UI
+                self.uiActivityIndicator.hidden = true
+                self.uiWebView.loadData(nsData!, MIMEType: "application/pdf", textEncodingName: "utf-8", baseURL: NSURL())
+            }
+            
+        }
+        nsURLSessionDataTask.resume()
+    }
+
     //MARK: Actions
     @IBAction func shareButton(sender: UIBarButtonItem) {
-        let textToShare = "Look at this awesome website for aspiring iOS Developers!"
-        let myWebsite = NSURL(string: "http://www.codingexplorer.com/")
-        
-        let objectsToShare:[AnyObject] = [textToShare, myWebsite!]
+//        let textToShare = "Look at this awesome website for aspiring iOS Developers!"
+//        let myWebsite = NSURL(string: "http://www.codingexplorer.com/")
+//        let objectsToShare:[AnyObject] = [textToShare, myWebsite!]
+        let objectsToShare:[AnyObject] = [nsData]
         let activityViewController = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
         
+        
         activityViewController.excludedActivityTypes = [UIActivityTypeAssignToContact,
+            UIActivityTypeCopyToPasteboard,
         UIActivityTypeSaveToCameraRoll,
         UIActivityTypeAddToReadingList,
         UIActivityTypePostToFlickr,
--- Readme.txt
+++ Readme.txt
@@ -5,3 +5,5 @@ Use UIActivityViewController to present the options for the 'share' button.
 
 References:
 http://www.codingexplorer.com/add-sharing-to-your-app-via-uiactivityviewcontroller/
+http://stackoverflow.com/questions/9569812/save-pdf-which-is-displayed-by-uiwebview-locally
+http://iosameer.blogspot.com.au/2012/08/reading-and-downloading-pdf-in-ios-app.html
